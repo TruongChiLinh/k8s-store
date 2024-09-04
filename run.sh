b9536c515ae1b4a40b8044c5692d893690a77a43
TruongChiLinh <63104230+TruongChiLinh@users.noreply.github.com>
2024-09-04 09:24:38 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,3 +5,7 @@ kubectl apply -f deployment.yml
 kubectl get ns practice-store 
 kubectl get deployments -n practice-store
 kubectl get pods -n practice-store
+
+kubectl describe pvc pvc-rwo -n practice-store
+
+kubectl get pvc -n practice-store
